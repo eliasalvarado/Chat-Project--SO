1cad2e93fab294fbcecd29547dad019802cfcf4d
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2024-05-26 14:35:22 -0600

    Client-Server changes

--- client.cpp
+++ client.cpp
@@ -1,30 +1,49 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <pthread.h>
+#include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "chat.pb.h"
 
-// Función para enviar la solicitud al servidor
-void send_request(const chat::Request& request, chat::Response& response, const std::string& server_ip, int server_port) {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+void broadcast_message(int sock);
+void send_private_message(int sock);
+void change_status(int sock);
+void list_users(int sock);
+void display_user_info(int sock);
+void display_help();
+void exit_chat(int sock);
+
+std::string username;
+
+bool register_user(const std::string& username, const std::string& server_ip, int server_port, int& sock) {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         std::cerr << "Error creating socket" << std::endl;
-        return;
+        return false;
     }
 
-    sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(server_port);
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(server_port);
 
-    if (inet_pton(AF_INET, server_ip.c_str(), &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, server_ip.c_str(), &server_address.sin_addr) <= 0) {
         std::cerr << "Invalid address/ Address not supported" << std::endl;
-        return;
+        return false;
     }
 
-    if (connect(sock, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection Failed" << std::endl;
-        return;
+    if (connect(sock, (sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        std::cerr << "Connection failed" << std::endl;
+        return false;
     }
+
+    chat::NewUserRequest new_user_request;
+    new_user_request.set_username(username);
+
+    chat::Request request;
+    request.set_operation(chat::Operation::REGISTER_USER);
+    *request.mutable_register_user() = new_user_request;
 
     std::string request_str;
     request.SerializeToString(&request_str);
@@ -33,10 +52,189 @@ void send_request(const chat::Request& request, chat::Response& response, const 
     char buffer[1024];
     int bytes_read = read(sock, buffer, sizeof(buffer));
     if (bytes_read > 0) {
+        chat::Response response;
         response.ParseFromArray(buffer, bytes_read);
+        if (response.status_code() == chat::StatusCode::OK) {
+            std::cout << "Response: " << response.message() << std::endl;
+            return true;
+        } else {
+            std::cout << "Response: " << response.message() << std::endl;
+            return false;
+        }
     }
 
+    return false;
+}
+
+void show_menu() {
+    std::cout << "Chat Menu:" << std::endl;
+    std::cout << "1. Chat with all users (broadcast)" << std::endl;
+    std::cout << "2. Send a private message" << std::endl;
+    std::cout << "3. Change status" << std::endl;
+    std::cout << "4. List connected users" << std::endl;
+    std::cout << "5. Display user information" << std::endl;
+    std::cout << "6. Help" << std::endl;
+    std::cout << "7. Exit" << std::endl;
+    std::cout << "Enter your choice: ";
+}
+
+void handle_choice(int choice, int sock) {
+    switch (choice) {
+        case 1:
+            broadcast_message(sock);
+            break;
+        case 2:
+            send_private_message(sock);
+            break;
+        case 3:
+            change_status(sock);
+            break;
+        case 4:
+            list_users(sock);
+            break;
+        case 5:
+            display_user_info(sock);
+            break;
+        case 6:
+            display_help();
+            break;
+        case 7:
+            exit_chat(sock);
+            break;
+        default:
+            std::cout << "Invalid choice. Please try again." << std::endl;
+    }
+}
+
+void broadcast_message(int sock) {
+    std::cout << "-----Broadcast message-----" << std::endl;
+    // Implementación de enviar mensaje broadcast
+}
+
+void send_private_message(int sock) {
+    std::cout << "-----Private message functionality-----" << std::endl;
+    // Implementación de enviar mensaje privado
+}
+
+void change_status(int sock) {
+    std::cout << "-----Change status functionality-----" << std::endl;
+
+    std::cout << "Select your new status:" << std::endl;
+    std::cout << "1. ONLINE" << std::endl;
+    std::cout << "2. BUSY" << std::endl;
+    std::cout << "3. OFFLINE" << std::endl;
+    std::cout << "Enter your choice: ";
+
+    int status_choice;
+    std::cin >> status_choice;
+
+    chat::UserStatus new_status;
+    switch (status_choice) {
+        case 1:
+            new_status = chat::UserStatus::ONLINE;
+            break;
+        case 2:
+            new_status = chat::UserStatus::BUSY;
+            break;
+        case 3:
+            new_status = chat::UserStatus::OFFLINE;
+            break;
+        default:
+            std::cout << "Invalid status choice. Please try again." << std::endl;
+            return;
+    }
+
+    chat::UpdateStatusRequest update_status_request;
+    update_status_request.set_username(username);
+    update_status_request.set_new_status(new_status);
+
+    chat::Request request;
+    request.set_operation(chat::Operation::UPDATE_STATUS);
+    *request.mutable_update_status() = update_status_request;
+
+    std::string request_str;
+    request.SerializeToString(&request_str);
+    send(sock, request_str.c_str(), request_str.size(), 0);
+
+    char buffer[1024];
+    int bytes_read = read(sock, buffer, sizeof(buffer));
+    if (bytes_read > 0) {
+        chat::Response response;
+        response.ParseFromArray(buffer, bytes_read);
+        if (response.status_code() == chat::StatusCode::OK) {
+            std::cout << "Status changed successfully to " << chat::UserStatus_Name(new_status) << std::endl;
+        } else {
+            std::cout << "Failed to change status: " << response.message() << std::endl;
+        }
+    }
+}
+
+void list_users(int sock) {
+    chat::Request request;
+    request.set_operation(chat::Operation::GET_USERS);
+
+    std::string request_str;
+    request.SerializeToString(&request_str);
+    send(sock, request_str.c_str(), request_str.size(), 0);
+
+    char buffer[1024];
+    int bytes_read = read(sock, buffer, sizeof(buffer));
+    if (bytes_read > 0) {
+        chat::Response response;
+        response.ParseFromArray(buffer, bytes_read);
+        std::cout << "-----Online users-----" << std::endl;
+        for (const auto& user : response.user_list().users()) {
+            std::cout << "Username: " << user.username() << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to receive response from server." << std::endl;
+    }
+}
+
+void display_user_info(int sock) {
+    std::cerr << "-----User Info-----" << std::endl;
+    std::cout << "Enter the username of the user you want to get information about: ";
+    std::string user_to_search;
+    std::cin >> user_to_search;
+
+    chat::Request request;
+    request.set_operation(chat::Operation::GET_USERS);
+    request.mutable_get_users()->set_username(user_to_search);
+
+    std::string request_str;
+    request.SerializeToString(&request_str);
+    send(sock, request_str.c_str(), request_str.size(), 0);
+
+    char buffer[1024];
+    int bytes_read = read(sock, buffer, sizeof(buffer));
+    if (bytes_read > 0) {
+        chat::Response response;
+        response.ParseFromArray(buffer, bytes_read);
+
+        if (response.user_list().users_size() > 0) {
+            const auto& user = response.user_list().users(0);
+            std::cout << user.username() << "'s information: " << std::endl;
+            std::cout << "Username: " << user.username() << std::endl;
+            std::cout << "IP Address: " << user.ip_address() << std::endl;
+            std::cout << "Status: " << chat::UserStatus_Name(user.status()) << std::endl;
+        } else {
+            std::cout << "User not found or not online." << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to receive response from server." << std::endl;
+    }
+}
+
+
+void display_help() {
+    std::cout << "-----Help functionality-----" << std::endl;
+    // Implementación de ayuda
+}
+
+void exit_chat(int sock) {
+    std::cout << "Exiting chat..." << std::endl;
     close(sock);
+    exit(0);
 }
 
 int main(int argc, char const* argv[]) {
@@ -45,23 +243,21 @@ int main(int argc, char const* argv[]) {
         return -1;
     }
 
-    std::string username = argv[1];
+    username = argv[1];
     std::string server_ip = argv[2];
     int server_port = std::stoi(argv[3]);
 
-    chat::Request request;
-    request.set_operation(chat::Operation::REGISTER_USER);
-    chat::NewUserRequest* new_user_request = request.mutable_register_user();
-    new_user_request->set_username(username);
-
-    chat::Response response;
-    send_request(request, response, server_ip, server_port);
-
-    if (response.status_code() == chat::StatusCode::OK) {
-        std::cout << "The User " << username << " has been registered successfully." << std::endl;
-    } else {
-        std::cout << "Error: " << response.message() << std::endl;
+    int sock;
+    if (!register_user(username, server_ip, server_port, sock)) {
+        std::cerr << "Failed to register user" << std::endl;
         return -1;
+    }
+
+    int choice;
+    while (true) {
+        show_menu();
+        std::cin >> choice;
+        handle_choice(choice, sock);
     }
 
     return 0;
--- server.cpp
+++ server.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <string>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <unordered_map>
 #include <pthread.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
 #include "chat.pb.h"
 
 struct UserSession {
     std::string username;
+    std::string ip_address;
     chat::UserStatus status;
     int socket;
+    std::chrono::system_clock::time_point last_activity;
 };
 
-std::map<std::string, UserSession> users;
+std::unordered_map<std::string, UserSession> users;
 pthread_mutex_t users_mutex = PTHREAD_MUTEX_INITIALIZER;
+int inactivity_timeout = 30;
 
-void print_registered_users() {
-    std::cout << "Registered Users:" << std::endl;
-    std::cout << "Users count: " << users.size() << std::endl;
-    for (const auto& user_pair : users) {
-        std::cout << "Username: " << user_pair.second.username << std::endl;
-    }
-    std::cout << "-----------------" << std::endl;
-}
-
-void handle_register_user(const chat::NewUserRequest& request, chat::Response& response) {
+void handle_register_user(const chat::NewUserRequest& request, chat::Response& response, int socket, const std::string& client_ip) {
     pthread_mutex_lock(&users_mutex);
     if (users.find(request.username()) != users.end()) {
         response.set_status_code(chat::StatusCode::BAD_REQUEST);
@@ -31,24 +28,84 @@ void handle_register_user(const chat::NewUserRequest& request, chat::Response& r
     } else {
         UserSession session;
         session.username = request.username();
+        session.ip_address = client_ip;
         session.status = chat::UserStatus::ONLINE;
-        // TODO: Set socket
+        session.socket = socket;
+        session.last_activity = std::chrono::system_clock::now();
 
         users[session.username] = session;
         response.set_status_code(chat::StatusCode::OK);
         response.set_message("User registered successfully");
+        
+        std::cout << "User registered: " << session.username << " with IP: " << session.ip_address << std::endl;
     }
-    print_registered_users();
     pthread_mutex_unlock(&users_mutex);
 }
 
-void handle_client(int client_socket) {
+void handle_update_status(const chat::UpdateStatusRequest& request, chat::Response& response) {
+    pthread_mutex_lock(&users_mutex);
+    auto it = users.find(request.username());
+    if (it != users.end()) {
+        it->second.status = request.new_status();
+        it->second.last_activity = std::chrono::system_clock::now(); 
+        response.set_status_code(chat::StatusCode::OK);
+        response.set_message("Status updated successfully");
+        std::cout << "User " << request.username() << " changed status to " << chat::UserStatus_Name(request.new_status()) << std::endl;
+    } else {
+        response.set_status_code(chat::StatusCode::BAD_REQUEST);
+        response.set_message("User not found");
+        
+        std::cout << "Failed to update status. Registered users:" << std::endl;
+        for (const auto& user : users) {
+            std::cout << "User: " << user.second.username << ", IP: " << user.second.ip_address << std::endl;
+        }
+        std::cout << "Attempted to change status for user: " << request.username() << std::endl;
+    }
+    pthread_mutex_unlock(&users_mutex);
+}
+
+void handle_client_disconnection(int client_sock) {
+    pthread_mutex_lock(&users_mutex);
+    for (auto it = users.begin(); it != users.end(); ++it) {
+        if (it->second.socket == client_sock) {
+            users.erase(it);
+            break;
+        }
+    }
+    pthread_mutex_unlock(&users_mutex);
+    close(client_sock);
+}
+
+void handle_get_users(const chat::UserListRequest& user_list_request, chat::UserListResponse* user_list_response) {
+    // std::cout << "handle get users: " << user_list_request.username() << std::endl;
+    if (user_list_request.username().empty()) {
+        for (const auto& user : users) {
+            if (user.second.status == chat::UserStatus::ONLINE) {
+                chat::User* user_proto = user_list_response->add_users();
+                user_proto->set_username(user.first);
+            }
+        }
+    } else {
+        auto it = users.find(user_list_request.username());
+        if (it != users.end()) {
+            user_list_response->clear_users();
+            chat::User* user_proto = user_list_response->add_users();
+            user_proto->set_username(it->first);
+            user_proto->set_ip_address(it->second.ip_address);
+            user_proto->set_status(it->second.status);
+        }
+    }
+}
+
+
+void handle_client(int socket) {
     char buffer[1024];
+
     while (true) {
-        int bytes_read = read(client_socket, buffer, sizeof(buffer));
+        int bytes_read = read(socket, buffer, sizeof(buffer));
         if (bytes_read <= 0) {
-            close(client_socket);
-            pthread_exit(NULL);
+            handle_client_disconnection(socket);
+            return;
         }
 
         chat::Request request;
@@ -57,25 +114,61 @@ void handle_client(int client_socket) {
         chat::Response response;
         switch (request.operation()) {
             case chat::Operation::REGISTER_USER:
-                handle_register_user(request.register_user(), response);
+                handle_register_user(request.register_user(), response, socket, "TODO: Get IP Address");
                 break;
-            // Add cases for other operations
+            case chat::Operation::UPDATE_STATUS:
+                handle_update_status(request.update_status(), response);
+                break;
+            case chat::Operation::GET_USERS:
+                handle_get_users(request.get_users(), response.mutable_user_list());
+                break;
             default:
                 response.set_status_code(chat::StatusCode::BAD_REQUEST);
                 response.set_message("Unknown operation");
         }
 
+        pthread_mutex_lock(&users_mutex);
+        for (auto& user : users) {
+            if (user.second.socket == socket && chat::Operation::UPDATE_STATUS != request.operation()) {
+                std::cout << "The user: " << user.second.username << " will be updated to ONLINE" << std::endl;
+                user.second.last_activity = std::chrono::system_clock::now();
+                user.second.status = chat::UserStatus::ONLINE;
+                break;
+            }
+        }
+        pthread_mutex_unlock(&users_mutex);
+
         std::string response_str;
         response.SerializeToString(&response_str);
-        send(client_socket, response_str.c_str(), response_str.size(), 0);
+        send(socket, response_str.c_str(), response_str.size(), 0);
     }
 
-    close(client_socket);
+    close(socket);
 }
 
+
 void* handle_client_wrapper(void* client_socket) {
-    handle_client(*(int*)client_socket);
-    delete (int*)client_socket;
+    int socket = *(int*)client_socket;
+    free(client_socket);
+    handle_client(socket);
+    return NULL;
+}
+
+void* inactivity_monitor(void*) {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        auto now = std::chrono::system_clock::now();
+
+        pthread_mutex_lock(&users_mutex);
+        for (auto& user : users) {
+            auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - user.second.last_activity).count();
+            if (duration >= inactivity_timeout && user.second.status != chat::UserStatus::OFFLINE) {
+                user.second.status = chat::UserStatus::OFFLINE;
+                std::cout << "User " << user.second.username << " set to OFFLINE due to inactivity" << std::endl;
+            }
+        }
+        pthread_mutex_unlock(&users_mutex);
+    }
     return NULL;
 }
 
@@ -96,17 +189,20 @@ int main(int argc, char const* argv[]) {
     bind(server_fd, (sockaddr*)&address, sizeof(address));
     listen(server_fd, 3);
 
-    printf("SUCCESS: listening on port-> %d\n", port);
+    pthread_t monitor_thread;
+    pthread_create(&monitor_thread, NULL, inactivity_monitor, NULL);
+
+    printf("The server is listening on port: %d\n", port);
 
     while (true) {
         sockaddr_in client_address;
         socklen_t client_addrlen = sizeof(client_address);
         int client_socket = accept(server_fd, (sockaddr*)&client_address, &client_addrlen);
 
-        int* pclient = new int;
-        *pclient = client_socket;
+        int* new_sock = new int;
+        *new_sock = client_socket;
         pthread_t thread_id;
-        pthread_create(&thread_id, NULL, handle_client_wrapper, (void*)pclient);
+        pthread_create(&thread_id, NULL, handle_client_wrapper, (void*)new_sock);
     }
 
     close(server_fd);
